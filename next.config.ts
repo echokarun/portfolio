cb8226f52397d3bbb8fdc1a43e13ef5c2deddde9
echokarun <rayamajhikarun@gmail.com>
2026-06-04 13:28:49 +0545

    explicitly recognize the export to be used for action runner file

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
+const isGithubPages = process.env.GITHUB_ACTIONS === "true";
+
 const nextConfig: NextConfig = {
+  output: isGithubPages ? "export" : undefined,
+  basePath: isGithubPages ? "/portfolio" : "",
   images: {
     unoptimized: true,
   },
